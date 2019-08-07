00513fc99ff62441e8742ececaef75f078255b04
feven tsegay <feventsegay6@gmail.com>
2019-08-07 11:26:08 -0500

    adding controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+    def index
+     render json: User.all
+    end
+
+    def create
+    user = User.create(users_params)
+    render json :user
+    end
+
+    def show
+      user = User.find(params[:id])
+      render json :user
+    end
+
+
+    def users_params
+        params.permit(:first_name,:password)
+    end
+
+    
+    
 end
--- db/migrate/20190807150011_create_users.rb
+++ db/migrate/20190807150011_create_users.rb
@@ -1,7 +1,16 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-
+      t.string :frist_name
+      t.string :last_name
+      t.string :password
+      t.string :gender
+      t.integer :age
+      t.string :zip_code
+      t.string :city
+      t.string :current_grade
+      t.string :school_id
+      t.string :favorite_id
       t.timestamps
     end
   end
