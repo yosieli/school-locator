c927d8184e42a2d270a67201a889b05af05e6f8a
feven tsegay <feventsegay6@gmail.com>
2019-08-07 18:11:41 -0500

    sign up created

---
+++ front-end/src/components/signIn.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import './signUp.css'
+export default class SignIn extends React.Component {
+
+    state={
+        first_name: '',
+        password: ''
+    }
+
+    handleChange = (e) => {
+        this.setState({
+           [e.target.name]: e.target.value 
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        fetch('http://localhost:3000/signup',{
+            method: 'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({
+                first_name: this.state.first_name,
+                password: this.state.password,
+
+            })
+        })
+        console.log('clicked')
+    }
+
+
+ render(){
+        return(
+
+        <div className="login-page">
+            <h1 > <strong> Welcome to school locator </strong> </h1>
+              
+                <div className="form">
+                <form onSubmit={this.handleSubmit} className="login-form">
+                <input name = "first_name"  type="name" placeholder="first_name" onChange={this.handleChange}/>
+                <input name = "password"    type = "name" placeholder = "password" onChange={this.handleChange}/>
+                  <button style={{"border-radius": "7px"}}>Sign In</button>
+                   
+                    <p className="message"> <h3>Not registered?</h3><a href="">Sign Up</a></p>
+                </form>
+                </div>
+                <h2> <strong> </strong> </h2>
+              </div>
+        )
+    }
+}
---
+++ front-end/src/components/signUp.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import './signUp.css'
+export default class SignUp extends React.Component {
+
+    state = {
+        first_name: '',
+        last_name: '',
+        password: '',
+        zip_code: '',
+        current_grade: ''
+
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        fetch('http://localhost:3000/signup',{
+            method: 'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({
+                first_name: this.state.first_name,
+                last_name: this.state.last_name,
+                password: this.state.password,
+                zip_code: this.state.zip_code,
+                current_grade: this.state.current_grade
+
+            })
+            })
+
+    }
+
+
+    handleChange = (e) => {
+      this.setState({
+            [e.target.name]: e.target.value
+      })
+
+    }
+
+    render(){
+        return(
+
+        <div className="login-page">
+            <h1 > <strong> Welcome to school locator </strong> </h1>
+              
+                <div className="form">
+                    <form onSubmit={this.handleSubmit} className="login-form">
+                    <input name = "first_name"  type="name" placeholder="first_name" onChange={this.handleChange}/>
+                    <input name = "last_name"    type = "name" placeholder = "last_name" onChange={this.handleChange}/>
+                    <input name = "password"  type="name" placeholder="password" onChange={this.handleChange}/>
+                    <input name = "zip_code"    type = "name" placeholder = "zip_code" onChange={this.handleChange}/>
+                    <input name = "current_grade"    type = "name" placeholder = "current_grade" onChange={this.handleChange}/>
+                    <button style={{"border-radius": "7px"}}>Sign Up</button>
+                    
+                        <p className="message"><h3>Already registered?</h3> <a href=""><h3>Sign In</h3></a></p>
+                    </form>
+                </div>
+                <h2> <strong> </strong> </h2>
+              </div>
+        )
+    }
+}
