032a0b8f4d6bd4c916d2f96f0dc0291d3a7e3362
feven tsegay <feventsegay6@gmail.com>
2019-08-07 11:27:17 -0500

    adding routes

--- back-end/app/controllers/favorites_controller.rb
+++ back-end/app/controllers/favorites_controller.rb
@@ -1,2 +1,15 @@
 class FavoritesController < ApplicationController
+    def index
+        render json: Favorite.all
+    end
+
+    def favorites
+        user = User.find(params[:id])
+        school = School.find(params[:id])
+        render json: user,school
+    end
+
+    
+
+
 end
--- back-end/db/migrate/20190807150133_create_favorites.rb
+++ back-end/db/migrate/20190807150133_create_favorites.rb
@@ -1,7 +1,11 @@
 class CreateFavorites < ActiveRecord::Migration[5.2]
   def change
     create_table :favorites do |t|
-
+      t.integer :user_id
+      t.integer :school_id
+      t.string :school_name
+      t.string :rating
+      t.string :zip_code
       t.timestamps
     end
   end
