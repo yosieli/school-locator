86078071d7ab7f6bb99daa838a4759ccd9c4ad65
feven tsegay <feventsegay6@gmail.com>
2019-08-08 10:16:40 -0500

    -

--- back-end/config/routes.rb
+++ back-end/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
+  # resources :users
   resources :favorites
   resources :schools
+
+  post '/signup', to: 'users#create'
+  get '/signin', to: 'users#authenticate'
+
 end
